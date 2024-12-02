acbb8d88b50196f87499c720ca35c29a2bd93189
patrick <anderspatricktakkis@gmail.com>
2024-12-02 21:18:46 +0200

    pealehe js

--- pealeht.js
+++ pealeht.js
@@ -1,28 +1,28 @@
-// Array of image paths and corresponding subpage links
+// Pildid mis viivad kasutaja teisele lingitud lehele
 const imageLinks = [
     { src: "pildid/Siidor.jpg", href: "Mirelle.html" },
     { src: "pildid/my_po.png", href: "Anne-Liine.html" },
     { src: "pildid/IMG_0443.jpg", href: "Patrick.html" }
   ];
   
-  // Get the container for images
+  // Piltide konteiner
   const container = document.getElementById("image-links");
   
-  // Loop through the images and create clickable links
+  
   imageLinks.forEach((item) => {
-    // Create an anchor element
+    // Uus element
     const link = document.createElement("a");
     link.href = item.href;
   
-    // Create an image element
+    // Pildi Element
     const image = document.createElement("img");
     image.src = item.src;
     image.alt = "Clickable image";
 
-    // Append the image to the link
+    // Link piltide jaoks
     link.appendChild(image);
   
-    // Append the link to the container
+    // Lingid konteinerisse
     container.appendChild(link);
   });
   
